16faf2d21182c3116a395c9a1de9860092849182
Владимир Толстов <vlatolstov@gmail.com>
2023-07-25 14:24:18 +0300

    Кейсы на месте, изменить метод

--- Program.cs
+++ Program.cs
@@ -8,14 +8,29 @@ namespace Ploskost_popolam
     {
         static void Main(string[] args)
         {
-            Point[] points1 = new Point[3] { new Point(1, 1), new Point(2, 2), new Point(3,3) };
-            Console.WriteLine(EqualHalfPlanes(points1));
+            Point[] points1 = new Point[3] { new Point(1, 1), new Point(2, 2), new Point(3, 3) };
+            Console.WriteLine(EqualHalfPlanes(points1) + " Answer is false");
 
             Point[] points2 = new Point[4] { new Point(1, 1), new Point(6, 1), new Point(0, 1), new Point(5, 1) };
-            Console.WriteLine(EqualHalfPlanes(points2));
+            Console.WriteLine(EqualHalfPlanes(points2) + " Answer is true");
 
             Point[] points3 = new Point[6] { new Point(1, 1), new Point(6, 1), new Point(0, 1), new Point(5, 1), new Point(4, 2), new Point(2, 3) };
-            Console.WriteLine(EqualHalfPlanes(points3));
+            Console.WriteLine(EqualHalfPlanes(points3) + " Answer is false");
+
+            Point[] points4 = new Point[4] { new Point(1, 1), new Point(6, 1), new Point(0, 1), new Point(5, 1) };
+            Console.WriteLine(EqualHalfPlanes(points4) + " Answer is true");
+
+            Point[] points5 = new Point[2] { new Point(1, 1), new Point(2, 2) };
+            Console.WriteLine(EqualHalfPlanes(points5) + " Answer is false");
+
+            Point[] points6 = new Point[2] { new Point(1, 2), new Point(5, 2) };
+            Console.WriteLine(EqualHalfPlanes(points6) + " Answer is true");
+
+            Point[] points7 = new Point[4] { new Point(1, 2), new Point(5, 2), new Point(4, 3), new Point(5, 3) };
+            Console.WriteLine(EqualHalfPlanes(points7) + " Answer is false");
+
+            Point[] points8 = new Point[3] { new Point(1, 2), new Point(5, 2), new Point(3, 3) };
+            Console.WriteLine(EqualHalfPlanes(points8) + " Answer is true");
         }
 
         static bool EqualHalfPlanes(Point[] points)
