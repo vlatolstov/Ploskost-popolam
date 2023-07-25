94f0fc526258beb677fc8b1700226258d7dda8d4
Владимир Толстов <vlatolstov@gmail.com>
2023-07-25 13:39:22 +0300

    Добавлена сортировка по point.X в последнем цикле

--- Program.cs
+++ Program.cs
@@ -11,10 +11,10 @@ namespace Ploskost_popolam
             Point[] points1 = new Point[2] { new Point(1, 2), new Point(5, 2) };
             Console.WriteLine(EqualHalfPlanes(points1));
 
-            Point[] points2 = new Point[4] { new Point(1, 2), new Point(5, 2), new Point(4,3), new Point(5,3) };
+            Point[] points2 = new Point[4] { new Point(1, 1), new Point(6, 1), new Point(0, 1), new Point(5, 1) };
             Console.WriteLine(EqualHalfPlanes(points2));
 
-            Point[] points3 = new Point[4] { new Point(1, 1), new Point(5, 1), new Point(-1,4), new Point(7,4)};
+            Point[] points3 = new Point[6] { new Point(4, 4), new Point(5, 0), new Point(12, -8), new Point(-3, 0), new Point(-10, -8), new Point(-2, 4) };
             Console.WriteLine(EqualHalfPlanes(points3));
         }
 
@@ -23,14 +23,14 @@ namespace Ploskost_popolam
             var averageX = points.Average(p => p.X);
             var leftPoints = points.Where(p => p.X <= averageX).OrderBy(p => p.Y);
             var rightPoints = points.Where(p => p.X > averageX).OrderBy(p => p.Y);
-            
+
             if (leftPoints.Count() != rightPoints.Count()
-                && leftPoints.Select(p => p.Y).SequenceEqual(rightPoints.Select(p => p.Y))) 
+                && leftPoints.Select(p => p.Y).SequenceEqual(rightPoints.Select(p => p.Y)))
                 return false;
 
             for (int i = 0; i < rightPoints.Count(); i++)
             {
-                if (averageX - leftPoints.ElementAt(i).X != rightPoints.ElementAt(i).X - averageX) return false;
+                if (averageX - leftPoints.OrderBy(p=>p.X).ElementAt(i).X != rightPoints.OrderByDescending(p => p.X).ElementAt(i).X - averageX) return false;
             }
             return true;
         }
