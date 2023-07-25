28a093280b8f35dd8b4c0efae2f0d72eb2b7c483
Владимир Толстов <vlatolstov@gmail.com>
2023-07-25 14:02:09 +0300

    Исправлено условие if

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ namespace Ploskost_popolam
     {
         static void Main(string[] args)
         {
-            Point[] points1 = new Point[2] { new Point(1, 2), new Point(5, 2) };
+            Point[] points1 = new Point[3] { new Point(1, 1), new Point(2, 2), new Point(3,3) };
             Console.WriteLine(EqualHalfPlanes(points1));
 
             Point[] points2 = new Point[4] { new Point(1, 1), new Point(6, 1), new Point(0, 1), new Point(5, 1) };
             Console.WriteLine(EqualHalfPlanes(points2));
 
-            Point[] points3 = new Point[6] { new Point(4, 4), new Point(5, 0), new Point(12, -8), new Point(-3, 0), new Point(-10, -8), new Point(-2, 4) };
+            Point[] points3 = new Point[6] { new Point(1, 1), new Point(6, 1), new Point(0, 1), new Point(5, 1), new Point(4, 2), new Point(2, 3) };
             Console.WriteLine(EqualHalfPlanes(points3));
         }
 
@@ -25,7 +25,7 @@ namespace Ploskost_popolam
             var rightPoints = points.Where(p => p.X > averageX).OrderBy(p => p.Y);
 
             if (leftPoints.Count() != rightPoints.Count()
-                && leftPoints.Select(p => p.Y).SequenceEqual(rightPoints.Select(p => p.Y)))
+                || !leftPoints.Select(p => p.Y).SequenceEqual(rightPoints.Select(p => p.Y)))
                 return false;
 
             for (int i = 0; i < rightPoints.Count(); i++)
